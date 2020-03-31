445e54d5480d07f6436a2aa4e6c2ae8a0f1386a0
eagleliujun <eagleliujun@hotmail.com>
2020-03-31 20:19:09 +0800

    2020-03-04

--- N326.py
+++ N326.py
@@ -19,6 +19,7 @@ Follow up:
 Could you do it without using any loop / recursion?
 
 """
+import math
 class Solution:
     def isPowerOfThree1(self, n: int) -> bool:
         if n == 1:
@@ -36,6 +37,16 @@ class Solution:
                 return False
             n = n/3
 
+    def isPowerOfThree3(self, n: int) -> bool:
+        n_set = {1,3,9,27,81,243,729,2187,6561,19683,59049,177147,531441,1594323,4782969,14348907,43046721,129140163,387420489,1162261467}
+        if n in n_set:
+            return True
+        return False
+
+    # def isPowerOfThree4(self, n: int) -> bool:
+    #     return False if n == 0 else pow(3, math.log(n, 3)) == n
+
+
     # def isPowerOfThree3(self, n: int) -> bool:  #
     #     a = list(str(n))
     #     if a == 3 or a == 1 : return True
@@ -50,9 +61,9 @@ class Solution:
     #         return True
 
 
-n=81
+n=243
 
 test = Solution()
 print(test.isPowerOfThree1(n))
 print(test.isPowerOfThree2(n))
-print(test.isPowerOfThree3(n))
+print(test.isPowerOfThree4(n))
