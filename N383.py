70448668810c1db854e92161aa8450fb22b904e6
eagleliujun <eagleliujun@hotmail.com>
2020-04-05 21:00:21 +0800

    2020-03-04

---
+++ N383.py
@@ -0,0 +1,50 @@
+"""
+Given an arbitrary ransom note string and another string containing letters from all the magazines,
+write a function that will return true if the ransom note can be constructed from the magazines ;
+otherwise, it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+
+"""
+
+import re
+class Solution:
+    def canConstruct1(self, ransomNote: str, magazine: str) -> bool: # str
+        for i in ransomNote:
+            if i not in magazine:
+                return False
+            magazine = magazine.replace(i,'?',1)
+        return True
+
+    def canConstruct2(self, ransomNote: str, magazine: str) -> bool: # list
+        ran_list , mag_list = list(ransomNote),list(magazine)
+        for i in ran_list:
+            if i not in mag_list:
+                return False
+            mag_list.remove(i)
+        return True
+
+    def canConstruct3(self, ransomNote: str, magazine: str) -> bool:  # dic
+        mag_dic = {}
+        for i in magazine:
+            mag_dic.update({i:mag_dic.setdefault(i,0)+1})
+        for j in ransomNote:
+            if j not in mag_dic.keys() or mag_dic[j] == 0:
+                return False
+            else:
+                mag_dic[j] -= 1
+        return True
+
+
+
+r = 'aa'
+m = 'aab'
+test = Solution()
+print(test.canConstruct3(r,m))
