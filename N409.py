b361f42215770e4bacf5287efad6f8bca40e6e2d
eagleliujun <eagleliujun@hotmail.com>
2020-04-13 20:20:53 +0800

    2020-04-10

--- N409.py
+++ N409.py
@@ -1,3 +1,22 @@
+"""
+Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes that can be built with those letters.
+
+This is case sensitive, for example "Aa" is not considered a palindrome here.
+
+Note:
+Assume the length of given string will not exceed 1,010.
+
+Example:
+
+Input:
+"abccccdd"
+
+Output:
+7
+
+Explanation:
+One longest palindrome that can be built is "dccaccd", whose length is 7.
+"""
 class Solution:
 
     def longestPalindrome1(self, s: str) -> int:
@@ -28,3 +47,14 @@ class Solution:
                 count +=1
                 break
         return count
+
+    def longestPalindrome3(self, s: str) -> int:
+        res = 0
+
+        for each in set(s):
+            res += (s.count(each)//2)*2
+
+        if res <len(s):
+            return res+1
+        else:
+            return res
